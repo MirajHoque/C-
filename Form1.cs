83fcc14ef5de8557f33e3939da1347b3a89420b4
MirajHoque <51967822+MirajHoque@users.noreply.github.com>
2019-10-26 00:37:51 +0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//using System.Data;
+
+namespace dbInformation
+{
+    public partial class Information : Form
+    {
+        public SqlConnection con=new SqlConnection(@" Data Source=DESKTOP-H57P91L\MIRAJSQL;Initial Catalog=TestDb;Integrated Security=True");
+        public SqlCommand cmd;
+        public SqlDataAdapter da;
+        public DataTable dt;
+      
+
+        
+        public Information()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into information values('" + txtName.Text + "','" + txtCity.Text + "','" + txtCountry.Text + "')";
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("seuccefully inserted");
+            con.Close();
+            displayData();
+
+        }
+        public void displayData()
+        {
+            con.Open();
+            cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = " select * from information";
+            cmd.ExecuteNonQuery();
+            dt = new DataTable();
+            da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            dgvinfo.DataSource = dt;
+            //cmd.ExecuteNonQuery();
+            con.Close();
+           // MessageBox.Show("seuccefully inserted");
+        }
+
+        private void Information_Load(object sender, EventArgs e)
+        {
+            displayData();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
